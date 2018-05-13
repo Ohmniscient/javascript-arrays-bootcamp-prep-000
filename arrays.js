1714c2dddba1a8b5a3a76d3ff4c58e7b6f84b42b
Cameron Orosco <cameron.orosco@outlook.com>
2018-05-13 06:34:29 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,7 +4,7 @@ function addElementToBeginningOfArray(array, element) {
   return [element, ...array]
 }
 
-function destructivelyAddElementToBeginningofArray(array, element) {
+function destructivelyAddElementToBeginningofArray('1') {
   array.unshift(element)
   return array
 }
